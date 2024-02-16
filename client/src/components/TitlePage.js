8440f6209df5400401bc0f9ab96542720178eca4
Matt <mburtonshaw@gmail.com>
2024-02-15 19:34:45 -0500

    TitlePage done

--- client/src/components/TitlePage.js
+++ client/src/components/TitlePage.js
@@ -4,25 +4,44 @@ export default function TitlePage(props) {
     let [movies, setMovies] = useState('');
     let [isLoading, setIsLoading] = useState(true);
 
-    function loader() {
+    async function getData() {
+        let films = await props.context.data.movies.movies;
+        setMovies(films);
+        setIsLoading(false);
+    }
+
+    useEffect(() => { getData() }, [setMovies]);
+
+    let films = props.context.data.movies.movies;
+    if (isLoading) {
         for (let i = 0; i < props.context.data.movies.movies.length; i++) {
-            return(
+
+            return (
                 <div>
-                    <h1>Liadughng</h1>
+                    <h1 className='pt-5 mt-5'>
+                        ...Loading...
+                    </h1>
+                    <div>
+                        <div className="card-group">
+                            {
+                                props.context.data.movies.movies.map((movie, i) => {
+                                    return (
+                                        <div key={i}>
+
+                                            <div className="card round_thumb_loader m-2">
+
+                                            </div>
+
+
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             );
         }
-    }
-
-    if (isLoading) {
-        return(
-            <div>
-                <h1 className='p-5'> ...loading... </h1>
-                <div>
-                    { loader() }
-                </div>
-            </div>
-        );
     } else {
         if (props.context.data.movies.movies.length < 1) {
             return (<h1>Not Found</h1>);
@@ -31,39 +50,38 @@ export default function TitlePage(props) {
                 if (window.innerWidth < 768) {
                     return (
                         <div>
-                            <h1>
+                            <h1 className='pt-5 mt-5'>
                                 Titles
                             </h1>
-                            <div>
-                                <div className="card-group">
-                                    {
-                                        props.context.data.movies.movies.map((movie, i) => {
-                                            return (
-                                                <div key={i}>
-                                                    <a href={`/titles/${movie.url}`}>
-                                                        <div className="card round_thumb">
-                                                            <img className="card-img-top round_thumb" src={`../../photos/titles/${movie.url}_round.jpg`} alt="Card image cap" />
-                                                        </div>
-                                                    </a>
-                                                    <p className='py-3'>{movie.title}</p>
-                                                </div>
-                                            );
-                                        })
-                                    }
-                                </div>
+
+                            <div className="card-group">
+                                {
+                                    props.context.data.movies.movies.map((movie, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <a href={`/titles/${movie.url}`}>
+                                                    <div className="card round_thumb">
+                                                        <img className="card-img-top round_thumb" src={`../../photos/titles/${movie.url}_round.jpg`} alt={`${movie.title} thumbnail`} />
+                                                    </div>
+                                                </a>
+                                                <p className='py-3'>{movie.title}</p>
+                                            </div>
+                                        );
+                                    })
+                                }
                             </div>
+
                         </div>
                     );
                 } else {
                     return (
                         <div className='m-auto'>
-                            <h1>
+                            <h1 className='pt-5 mt-5'>
                                 Titles
                             </h1>
                             <div className="card-group">
                                 {
                                     props.context.data.movies.movies.map((movie, i) => {
-
                                         return (
                                             <div key={i}>
                                                 <a href={`/titles/${movie.url}`}>
